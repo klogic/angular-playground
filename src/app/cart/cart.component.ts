335b737eeb42f99ff01526207bbe69fc26eb0224
klogic <klogic@hotmail.co.th>
2023-06-11 19:03:23 +0700

    submit application and clear form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +8,22 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private fromBuilder: FormBuilder
+  ) {}
 
   items = this.cartService.getItems();
+
+  checkoutForm = this.fromBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  onSubmit(): void {
+    console.warn('you item is', this.items);
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
